3badb473125cc7ced03638a2924fd65138975dca
Gabriel Villagran <metalmadnes98@gmail.com>
2025-05-30 10:18:29 -0400

    first commit

---
+++ SwiftUIChallenge/SwiftUIChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChallengeApp.swift
+//  SwiftUIChallenge
+//
+//  Created by GenericDevGeorgia on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
